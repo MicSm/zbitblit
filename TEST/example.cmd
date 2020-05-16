a849762b87b346910d869dfb4293ec975b817700
Michael S <mikhail.semikov@gmail.com>
2020-05-16 22:13:32 +0300

    Patch

--- TEST/example.cmd
+++ TEST/example.cmd
@@ -1,8 +1,8 @@
 @rem let's compress the "sources.tar" file, decompress it again and check file integrity
 
-ecp -p sources.tar
+zbitblit -p sources.tar
 @ren sources.tar sources.tar.1
-@ecp -d sources.tar.ecp
+@zbitblit -d sources.tar.zbb
 @fc /B sources.tar sources.tar.1
 @del sources.tar.1 
 @pause
