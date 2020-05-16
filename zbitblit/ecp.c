f385009a1b8930678084ec07506bc164fc36be86
Michael S <mikhail.semikov@gmail.com>
2020-05-16 22:44:23 +0300

    error

--- zbitblit/ecp.c
+++ zbitblit/ecp.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- *   Copyright (C) 1999-2000 Semikov Michael Alexandrovitch                 *
+ *   Copyright (C) 1999-2020 Semikov Michael Alexandrovitch                 *
  *                                                                          *
  *   This program is free software; you can redistribute it and/or modify   *
  *   it under the terms of the GNU General Public License as published by   *
