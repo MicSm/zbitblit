78741331b8946563fdd0b004c0d63612b0c1d6c4
Mike <mikhail.semikov@gmail.com>
2025-05-08 13:54:03 +0300

    mtf

--- src/mtf.c
+++ src/mtf.c
@@ -1,49 +1,65 @@
 #include "inc/mtf.h"
 
-uint16_t MtfLinks[256];
-uint16_t HeadPtr;
+typedef struct
+{
+	union
+	{
+		uint16_t MtfLinks[256];
+		uint8_t DeMtfArray[256];
+	} data;
 
-uint8_t* DeMtfArray;
+	uint16_t HeadPtr;
 
-void MtfSetup(void) {
-	uint16_t i;
-	HeadPtr = 0;
-	for (i = 0; i < 256; i++)
-		MtfLinks[i] = (uint16_t)(i + 1);
+} MtfState;
+
+static MtfState mtf_state;
+
+void MtfSetup(void)
+{
+	mtf_state.HeadPtr = 0;
+	for (uint16_t i = 0; i < 256; i++)
+	{
+		mtf_state.data.MtfLinks[i] = (uint16_t)(i + 1);
+	}
 }
 
-void DeMtfSetup(void) {
-	uint16_t i;
-	DeMtfArray = (uint8_t*)MtfLinks;
-	for (i = 0; i < 256; i++) DeMtfArray[i] = (uint8_t)i;
+void DeMtfSetup(void)
+{
+	for (uint16_t i = 0; i < 256; i++)
+	{
+		mtf_state.data.DeMtfArray[i] = (uint8_t)i;
+	}
 }
 
-uint16_t GetMtfValue(uint16_t InValue) {
-	uint16_t SkippedNums, p, PredPtr;
+uint16_t GetMtfValue(uint16_t InValue)
+{
+	uint16_t SkippedNums = 0, p = 0, PredPtr = 0;
 
 	SkippedNums = 0;
-	p = HeadPtr;
+	p = mtf_state.HeadPtr;
 	while (p != InValue) {
 		PredPtr = p;
-		p = MtfLinks[p];
+		p = mtf_state.data.MtfLinks[p];
 		SkippedNums++;
 	}
 	if (SkippedNums) {
-		MtfLinks[PredPtr] = MtfLinks[p];
-		MtfLinks[p] = HeadPtr;
-		HeadPtr = p;
+		mtf_state.data.MtfLinks[PredPtr] = mtf_state.data.MtfLinks[p];
+		mtf_state.data.MtfLinks[p] = mtf_state.HeadPtr;
+		mtf_state.HeadPtr = p;
 	}
 	return SkippedNums;
 }
 
-uint8_t GetByMtfPosition(uint8_t Position) {
-	uint8_t Result;
-	uint8_t i;
+uint8_t GetByMtfPosition(uint8_t Position)
+{
+	uint8_t Result = mtf_state.data.DeMtfArray[Position];
 
-	Result = DeMtfArray[Position];
 	if (Position != 0) {
-		for (i = Position; i > 0; i--) DeMtfArray[i] = DeMtfArray[i - 1];
-		DeMtfArray[0] = Result;
+		for (uint8_t i = Position; i > 0; i--)
+		{
+			mtf_state.data.DeMtfArray[i] = mtf_state.data.DeMtfArray[i - 1];
+		}
+		mtf_state.data.DeMtfArray[0] = Result;
 	}
 	return Result;
 }
